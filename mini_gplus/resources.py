1e33e1ffea7e8a2410e882b8f8f086cfabaf10cc
KTachibanaM <whj19931115@gmail.com>
2021-07-25 01:28:11 -0700

    marshal posts

--- mini_gplus/resources.py
+++ mini_gplus/resources.py
@@ -1,4 +1,4 @@
-from flask_restful import reqparse, Resource
+from flask_restful import reqparse, Resource, fields, marshal_with
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from .models import User
 
@@ -7,18 +7,21 @@ user_parser = reqparse.RequestParser()
 user_parser.add_argument('id', type=str, required=True)
 user_parser.add_argument('password', type=str, required=True)
 
+user_fields = {
+    'id': fields.String(attribute='user_id'),
+    'created_at_seconds': fields.Integer(attribute='created_at_unix_seconds')
+}
+
 
 class UserList(Resource):
     @jwt_required()
+    @marshal_with(user_fields)
     def get(self):
         """
         Get all users other than the logged in user
         """
         user_id = get_jwt_identity()
-        other_users = [
-            {'id': user.user_id, 'createdAtSeconds': user.created_at_unix_seconds}
-            for user in User.objects(user_id__ne=user_id)
-        ]
+        other_users = [_ for _ in User.objects(user_id__ne=user_id)]
         return other_users, 200
 
 
@@ -26,9 +29,19 @@ post_parser = reqparse.RequestParser()
 post_parser.add_argument('content', type=str, required=True)
 
 
+post_fields = {
+    'author': fields.Nested(user_fields),
+    'content': fields.String,
+    'is_public': fields.Boolean,
+}
+
+
 class PostList(Resource):
     @jwt_required()
     def post(self):
+        """
+        Creates a new post
+        """
         user_id = get_jwt_identity()
         user = User.find(user_id)
 
@@ -40,6 +53,18 @@ class PostList(Resource):
             # TODO: change
             circles=[]
         )
+
+    @jwt_required()
+    @marshal_with(post_fields)
+    def get(self):
+        """
+        Get posts that are visible to the current user
+        """
+        user_id = get_jwt_identity()
+        user = User.find(user_id)
+
+        posts = user.sees_posts()
+        return posts, 200
 
 
 class Me(Resource):
