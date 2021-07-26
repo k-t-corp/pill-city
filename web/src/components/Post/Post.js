7d86a00ddd6a61a84c4e2242e2a586861a17f416
innkuika <innku.ika@gmail.com>
2021-07-26 01:10:56 -0700

    time delta css

--- web/src/components/Post/Post.js
+++ web/src/components/Post/Post.js
@@ -6,14 +6,15 @@ export default (props) => {
   const timePosted = (postedAtSeconds) => {
     const currentTimeAtSeconds = new Date().getTime() / 1000;
     const deltaAtSeconds = currentTimeAtSeconds - postedAtSeconds
+    console.log(deltaAtSeconds, currentTimeAtSeconds, postedAtSeconds)
     if (deltaAtSeconds < 60) {
-      return `${deltaAtSeconds}s`
+      return `${Math.floor(deltaAtSeconds)}s`
     } else if (deltaAtSeconds < 3600) {
-      return `${Math.floor(deltaAtSeconds/60)}m`
+      return `${Math.floor(deltaAtSeconds / 60)}m`
     } else if (deltaAtSeconds < 3600 * 24) {
-      return `${Math.floor(deltaAtSeconds/3600)}h`
+      return `${Math.floor(deltaAtSeconds / 3600)}h`
     } else if (deltaAtSeconds < 3600 * 24 * 7) {
-      return `${Math.floor(deltaAtSeconds/(3600 * 24))}d`
+      return `${Math.floor(deltaAtSeconds / (3600 * 24))}d`
     } else {
       return new Date(postedAtSeconds).toISOString().split('T')[0];
     }
@@ -66,8 +67,7 @@ export default (props) => {
               {comment.author.id}
             </div>
             <div className="post-time">
-              {timePosted(comment.author.created_at_seconds)}
-              39d
+              {timePosted(comment.created_at_seconds)}
             </div>
           </div>
           <div className="post-content comment-content">
@@ -94,19 +94,23 @@ export default (props) => {
     window.location.reload()
   }
 
-
   return (
     <div className="post-wrapper">
       <div className="post-op">
         <div className="post-op-info-wrapper">
-          <div className="post-avatar">
-            <img className="post-avatar-img" src={`${process.env.PUBLIC_URL}/kusuou.PNG`} alt=""/>
+          <div className="post-op-info-left">
+            <div className="post-avatar">
+              <img className="post-avatar-img" src={`${process.env.PUBLIC_URL}/kusuou.PNG`} alt=""/>
+            </div>
+            <div className="post-name">
+              {props.data.author.id}
+            </div>
+            <div className="post-visibility">
+              &#x25B8; {props.data.is_public ? "Public" : props.data.circles.join(", ")}
+            </div>
           </div>
-          <div className="post-name">
-            {props.data.author.id}
-          </div>
-          <div className="post-visibility">
-            &#x25B8; {props.data.is_public ? "Public" : props.data.circles.join(", ")}
+          <div className="post-op-info-right">
+            34d
           </div>
         </div>
         <div className="post-content">
