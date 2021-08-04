fb5b2fdaf8a6b485ed2cfbd103b69e771ccc0670
KTachibanaM <whj19931115@gmail.com>
2021-08-03 23:41:42 -0700

    update heroku with env vars for s3

--- infrastructure/heroku.tf
+++ infrastructure/heroku.tf
@@ -5,6 +5,13 @@ data "heroku_app" "app" {
 resource "heroku_config" "app-config" {
   sensitive_vars = {
     MONGODB_URI = local.mongodb_rw_connection_string
+    S3_ENDPOINT_URL = "https://s3.us-west-2.amazonaws.com"
+    S3_REGION = "us-west-2"
+    S3_ACCESS_KEY = aws_iam_access_key.stsadmin-secret.id
+    S3_SECRET_KEY = aws_iam_access_key.stsadmin-secret.secret
+    S3_BUCKET_NAME = "pill-city"
+    # TODO: use cloudfront
+    CDN_URL = "https://pill-city.s3.us-west-2.amazonaws.com"
   }
 }
 
