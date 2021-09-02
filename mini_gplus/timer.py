86f7cff593744c459bd89048e8779c4affe1c7f2
KTachibanaM <whj19931115@gmail.com>
2021-09-02 15:21:43 -0700

    try time ns

--- mini_gplus/timer.py
+++ mini_gplus/timer.py
@@ -3,9 +3,9 @@ import time
 
 class Timer(object):
     def __enter__(self):
-        self.start = round(time.time() * 1000)
+        self.start = time.time_ns() // 1000000
         return self
 
     def __exit__(self, *args):
-        self.end = round(time.time() * 1000)
+        self.end = time.time_ns() // 1000000
         self.interval = self.end - self.start
