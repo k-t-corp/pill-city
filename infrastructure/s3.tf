6bd27de20ee01bf8f0e5cd948aa0a8b043c16c18
KTachibanaM <whj19931115@gmail.com>
2021-09-02 13:26:25 -0700

    ahh

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -74,22 +74,15 @@ resource "aws_iam_role" "media-reader" {
 
   inline_policy {
     name = "media-reader-policy"
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
         {
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:s3:::pill-city/media/*"
-            ],
-            "Sid": ""
+          Action = "s3:GetObject"
+          Effect = "Allow"
+          Resource = "arn:aws:s3:::pill-city/media/*"
         }
-    ]
-}
-EOF
+      ]
+    })
   }
 }
