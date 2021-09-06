44bfd8a98413c748b51bb5af15eb966ddc120e63
KTachibanaM <whj19931115@gmail.com>
2021-09-05 18:34:58 -0700

    freeze time pag testes

--- mini_gplus/tests/daos/pagination_test.py
+++ mini_gplus/tests/daos/pagination_test.py
@@ -1,4 +1,5 @@
 from .base_test_case import BaseTestCase
+from freezegun import freeze_time
 from mini_gplus.models import Post
 from mini_gplus.daos.user import sign_up, find_user
 from mini_gplus.daos.post import create_post, get_post, sees_post
@@ -61,3 +62,56 @@ class PaginationTest(BaseTestCase):
         last_post = all_paged_posts[4]
         self.assertEqual(all_paged_posts[5:], get_page(Post, {}, self._post_filter_noop, last_post.created_at_ms,
                                                        last_post.eid, 5))
+
+    def test_multiple_pages_no_time_collision_filter(self):
+        self.assertTrue(sign_up('user1', '1234'))
+        self.assertTrue(sign_up('user2', '1234'))
+        user1 = find_user('user1')
+        user2 = find_user('user2')
+        all_paged_posts = []
+        for i in range(18):
+            if i % 2 == 0:
+                all_paged_posts.append(get_post(create_post(user1, str(i), True, [], False, None, [])))
+            else:
+                create_post(user2, str(i), True, [], False, None, [])
+        all_paged_posts = list(reversed(all_paged_posts))
+
+        self.assertEqual(all_paged_posts[: 5], get_page(Post, {}, self._post_filter_sees_on_home(user1), None, None, 5))
+        last_post = all_paged_posts[4]
+        self.assertEqual(all_paged_posts[5:], get_page(Post, {}, self._post_filter_sees_on_home(user1),
+                                                       last_post.created_at_ms, last_post.eid, 5))
+
+    def test_multiple_pages_time_collision_no_filter(self):
+        self.assertTrue(sign_up('user1', '1234'))
+        user1 = find_user('user1')
+        all_paged_posts = []
+        with freeze_time():
+            for i in range(9):
+                all_paged_posts.append(get_post(create_post(user1, str(i), True, [], False, None, [])))
+        all_paged_posts = list(reversed(all_paged_posts))
+        self.assertEqual(1, len(set(map(lambda p: p.created_at_ms, all_paged_posts))))
+
+        self.assertEqual(all_paged_posts[: 5], get_page(Post, {}, self._post_filter_noop, None, None, 5))
+        last_post = all_paged_posts[4]
+        self.assertEqual(all_paged_posts[5:], get_page(Post, {}, self._post_filter_noop, last_post.created_at_ms,
+                                                       last_post.eid, 5))
+
+    def test_multiple_pages_time_collision_filter(self):
+        self.assertTrue(sign_up('user1', '1234'))
+        self.assertTrue(sign_up('user2', '1234'))
+        user1 = find_user('user1')
+        user2 = find_user('user2')
+        all_paged_posts = []
+        with freeze_time():
+            for i in range(18):
+                if i % 2 == 0:
+                    all_paged_posts.append(get_post(create_post(user1, str(i), True, [], False, None, [])))
+                else:
+                    create_post(user2, str(i), True, [], False, None, [])
+        all_paged_posts = list(reversed(all_paged_posts))
+        self.assertEqual(1, len(set(map(lambda p: p.created_at_ms, all_paged_posts))))
+
+        self.assertEqual(all_paged_posts[: 5], get_page(Post, {}, self._post_filter_sees_on_home(user1), None, None, 5))
+        last_post = all_paged_posts[4]
+        self.assertEqual(all_paged_posts[5:], get_page(Post, {}, self._post_filter_sees_on_home(user1),
+                                                       last_post.created_at_ms, last_post.eid, 5))
