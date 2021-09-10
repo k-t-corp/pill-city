d3fdeb08c713429d432ae031c579727b5e502a15
KTachibanaM <whj19931115@gmail.com>
2021-09-10 01:51:35 -0700

    embed reactions and notifications into post

--- release.py
+++ release.py
@@ -3,6 +3,7 @@ from pymongo.uri_parser import parse_uri
 from mongoengine import connect
 from mini_gplus.daos.notification import backfill_notifications_eid
 from mini_gplus.daos.circle import backfill_circles_eid
+from mini_gplus.models.post import backfill_posts_embed_reactions_and_comments
 
 uri = os.environ['MONGODB_URI']
 connect(
@@ -11,3 +12,4 @@ connect(
 )
 backfill_notifications_eid()
 backfill_circles_eid()
+backfill_posts_embed_reactions_and_comments()
