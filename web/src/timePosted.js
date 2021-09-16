873ba693a8799fcdee008729f033108aabdae6a9
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-09-16 18:40:45 +0800

    refactor with conditional (ternary) operator

--- web/src/timePosted.js
+++ web/src/timePosted.js
@@ -1,15 +1,12 @@
 export default (postedAtSeconds) => {
+  const minute = 60, hour = 3600, day = 86400, week = 604800;
   const currentTimeAtSeconds = new Date().getTime() / 1000;
-  const deltaAtSeconds = currentTimeAtSeconds - postedAtSeconds
-  if (deltaAtSeconds < 60) {
-    return `${Math.floor(deltaAtSeconds)}s`
-  } else if (deltaAtSeconds < 3600) {
-    return `${Math.floor(deltaAtSeconds / 60)}m`
-  } else if (deltaAtSeconds < 3600 * 24) {
-    return `${Math.floor(deltaAtSeconds / 3600)}h`
-  } else if (deltaAtSeconds < 3600 * 24 * 7) {
-    return `${Math.floor(deltaAtSeconds / (3600 * 24))}d`
-  } else {
-    return new Date(postedAtSeconds * 1000).toISOString().split('T')[0];
-  }
+  const deltaAtSeconds = currentTimeAtSeconds - postedAtSeconds;
+  const d = deltaAtSeconds;
+  return
+    d < minute ? `${Math.floor(d)}s`
+    : d < hour ? `${Math.floor(d / minute)}m`
+    : d < day  ? `${Math.floor(d / hour)}h`
+    : d < week ? `${Math.floor(d / day)}d`
+    : new Date(postedAtSeconds * 1000).toISOString().split('T')[0];
 }
