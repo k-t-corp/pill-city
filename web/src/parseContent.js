e50a2bea26d834ac7e110b6ecdac09ab1b6443e2
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-09-16 18:40:36 +0800

    fix regexp, use chained replace()

--- web/src/parseContent.js
+++ web/src/parseContent.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 export default (content, className) => {
-  const regExForStrikeThrough = / -(.+)- /g
-  const regExForItalic = / _(.+)_ /g
-  const regExForBold = / \*(.+)\* /g
-  const regExForMention = /@([A-Za-z0-9_-]+) /g;
-  let newContent = content.replace(regExForStrikeThrough, '<del>$1</del>');
-  newContent = newContent.replace(regExForItalic, '<i>$1</i>')
-  newContent = newContent.replace(regExForBold, '<b>$1</b>')
-  newContent = newContent.replace(regExForMention, `<span style="color: #56a5ff" onClick="window.location.href='/profile/$1'">@$1&nbsp;</span>`)
+
+  /* const [
+    regExForStrikeThrough,
+    regExForItalic,
+    regExForBold
+  ] = [ "-", "_", "\\*" ].map(
+    char => new RegExp(`(?<=\\s|^)${char}(.+?)${char}(?=\\s|$)`, "g")
+  ); */
+
+  const regExForStrikeThrough = /(?<=\s|^)-(.+?)-(?=\s|$)/g
+  const regExForItalic = /(?<=\s|^)_(.+?)_(?=\s|$)/g
+  const regExForBold = /(?<=\s|^)\*(.+?)\*(?=\s|$)/g
+  const regExForMention = /@([A-Za-z0-9_-]+)(?=\s|$)/g;
+
+  const newContent = content
+    .replace(regExForStrikeThrough, '<del>$1</del>')
+    .replace(regExForItalic, '<i>$1</i>')
+    .replace(regExForBold, '<b>$1</b>')
+    .replace(
+      regExForMention,
+      `<a href="/profile/$1" style="color: #56a5ff; text-decoration: none;">@$1&nbsp;</a>`
+    );
+
   return <div className={className} dangerouslySetInnerHTML={{__html: newContent}}/>
+
 }
