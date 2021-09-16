bf95af20606c2281ca201b278aeb138a5487e196
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-09-16 18:41:34 +0800

    replace Boolean() with comparison

--- web/src/registerServiceWorker.js
+++ web/src/registerServiceWorker.js
@@ -8,21 +8,20 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+const isLocalhost =
+  location.hostname === 'localhost' ||
+  // [::1] is the IPv6 localhost address.
+  location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  ) !== null;
 
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
-    if (publicUrl.origin !== window.location.origin) {
+    const publicUrl = new URL(process.env.PUBLIC_URL, location);
+    if (publicUrl.origin !== location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
