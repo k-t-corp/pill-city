89282f1f97c3e25c03871de6d8001c4be0d3969b
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-09-17 15:56:35 +0800

    add parentheses

--- web/src/timePosted.js
+++ web/src/timePosted.js
@@ -3,10 +3,11 @@ export default (postedAtSeconds) => {
   const currentTimeAtSeconds = new Date().getTime() / 1000;
   const deltaAtSeconds = currentTimeAtSeconds - postedAtSeconds;
   const d = deltaAtSeconds;
-  return
+  return (
     d < minute ? `${Math.floor(d)}s`
     : d < hour ? `${Math.floor(d / minute)}m`
     : d < day  ? `${Math.floor(d / hour)}h`
     : d < week ? `${Math.floor(d / day)}d`
-    : new Date(postedAtSeconds * 1000).toISOString().split('T')[0];
+    : new Date(postedAtSeconds * 1000).toISOString().split('T')[0]
+  );
 }
