72f543a3c46a924ad8e64e93cd1e482fcb5efc06
KTachibanaM <whj19931115@gmail.com>
2021-09-17 02:56:05 -0700

    you may hit a bug is not accurate

--- web/src/components/About/About.js
+++ web/src/components/About/About.js
@@ -12,11 +12,7 @@ export default (props) => {
     return <a href={`https://github.com/KTachibanaM/pill-city/commit/${commit}`} className='about-commit-link'>{commit}</a>
   }
 
-  const youMayHitABug = () => {
-    return <span>. <span className='about-you-may-hit-a-bug'>You may hit a bug</span></span>
-  }
-
   return (
-    <p className='about'>Web {webGitCommit ? githubLink(webGitCommit) : '?'}, API {apiGitCommit ? githubLink(apiGitCommit) : '?'}{webGitCommit !== apiGitCommit ? youMayHitABug() : null}</p>
+    <p className='about'>Web {webGitCommit ? githubLink(webGitCommit) : '?'}, API {apiGitCommit ? githubLink(apiGitCommit) : '?'}</p>
   )
 }
