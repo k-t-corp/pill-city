e2e9748a126d34dc644225cc66b5a3aee498ab5e
KTachibanaM <whj19931115@gmail.com>
2021-09-17 17:27:21 -0700

    ui for quick following'

--- web/src/pages/Users/Users.js
+++ web/src/pages/Users/Users.js
@@ -1,78 +1,102 @@
-import React, {Component} from 'react'
-import {Loader, Message} from "semantic-ui-react";
-import CatchApiErrorBuilder from "../../api/CatchApiErrorBuilder";
+import React, {useEffect, useState} from 'react'
 import "./Users.css"
 import getAvatarUrl from "../../api/getAvatarUrl";
 
-export default class Users extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      'loading': true,
-      'error': undefined,
-      'data': undefined
+export default (props) => {
+  const [users, updateUsers] = useState([])
+  useEffect(async () => {
+    const users = await props.api.getUsers()
+    updateUsers(users.map(u => {
+      return {...u, _follow_loading: false}
+    }))
+  }, [])
+
+  let userCardElements = []
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i]
+    const userCardOnClick = () => {
+      window.location.href = `/profile/${user.id}`
     }
-  }
-
-  showError = (err) => {
-    this.setState({'error': err.toString()})
-  }
-
-  componentDidMount() {
-    this.props.api.getUsers()
-      .then(data => {
-        this.setState({'data': data})
-      })
-      .catch(
-        new CatchApiErrorBuilder()
-          .unknownError(this.showError)
-          .unknownStatusCode(this.showError)
-          .build()
-      ).finally(() => {
-      this.setState({'loading': false})
-    })
-  }
-
-  render() {
-    if (this.state.loading) {
-      return (
-        <Loader size='massive'/>
-      )
-    }
-    if (this.state.error) {
-      return (
-        <Message negative>{this.state.error}</Message>
-      )
-    }
-
-    const users = this.state.data
-    let userCardElements = []
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i]
-      const userCardOnClick = () => {
-        window.location.href = `/profile/${user.id}`
-      }
-      // const createdAtDate = new Date(user['created_at_seconds'] * 1000)
-      userCardElements.push(
-        <div className="users-user-card-wrapper" key={i} onClick={userCardOnClick}>
-          <div className="users-user-card-avatar">
-            <img className="users-user-card-avatar-img"  src={getAvatarUrl(user)} alt=""/>
-          </div>
+    const createdAtDate = new Date(user['created_at_seconds'] * 1000)
+    userCardElements.push(
+      <div className="users-user-card-wrapper" key={i} onClick={userCardOnClick}>
+        <div className="users-user-card-avatar">
+          <img className="users-user-card-avatar-img" src={getAvatarUrl(user)} alt=""/>
+        </div>
+        <div className='users-user-card-right'>
+          <div>
             <div className="users-user-card-name">
               {user.id}
             </div>
-          {/*Will cause overflow. let's deal with this later*/}
-          {/*<div className="users-user-card-join-time">*/}
-          {/*  Joined on {createdAtDate.toLocaleDateString()}*/}
-          {/*</div>*/}
+            <div className="users-user-card-join-time">
+              Joined on {createdAtDate.toLocaleDateString(undefined, {year: '2-digit', month: 'short', day: 'numeric'})}
+            </div>
+          </div>
+          <div
+            className={
+              !user._follow_loading ?
+                "users-user-card-follow-button" :
+                "users-user-card-follow-button users-user-card-follow-button-disabled"
+            }
+            onClick={async (e) => {
+              e.stopPropagation()
+              if (user._follow_loading) {
+                return
+              }
+              updateUsers(users.map(u => {
+                if (u.id !== user.id) {
+                  return u
+                }
+                return {
+                  ...u,
+                  _follow_loading: true
+                }
+              }))
+              if (user.is_following) {
+                await props.api.unfollow(user.id)
+              } else {
+                await props.api.follow(user.id)
+              }
+              updateUsers(users.map(u => {
+                if (u.id !== user.id) {
+                  return u
+                }
+                return {
+                  ...u,
+                  is_following: !u.is_following,
+                  _follow_loading: false
+                }
+              }))
+            }}
+          >
+            {
+              user.is_following ?
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M11 6a3 3 0 11-6 0 3 3 0 016 0zM14 17a6 6 0 00-12 0h12zM13 8a1 1 0 100 2h4a1 1 0 100-2h-4z"/>
+                </svg> :
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                        d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"/>
+                </svg>
+            }
+          </div>
         </div>
-      )
-    }
-
-    return (
-      <div className="users-grid-container">
-        {userCardElements}
       </div>
     )
   }
+
+  return (
+    <div className="users-grid-container">
+      {userCardElements}
+    </div>
+  )
 }
