61e932ff40ec900ffa6ef99e1a391344da81cc17
innkuika <innku.ika@gmail.com>
2021-09-18 13:06:10 -0700

    update parse content regex

--- web/src/parseContent.js
+++ web/src/parseContent.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default (content, className) => {
 
-  const regExForStrikeThrough = /-(.+?)-/g
-  const regExForItalic = /_(.+?)_/g
-  const regExForBold = /\*(.+?)\*/g
+  const regExForStrikeThrough = /(^|\s)-(.+?)-($|\s)/g
+  const regExForItalic = /(^|\s)_(.+?)_($|\s)/g
+  const regExForBold = /(^|\s)\*(.+?)\*($|\s)/g
   const regExForMention = /@([A-Za-z0-9_-]+)(\s|$)/g;
 
   const newContent = content
-    .replace(regExForStrikeThrough, '<del>$1</del>')
-    .replace(regExForItalic, '<i>$1</i>')
-    .replace(regExForBold, '<b>$1</b>')
+    .replace(regExForStrikeThrough, '$1<del>$2</del>$3')
+    .replace(regExForItalic, '$1<i>$2</i>$3')
+    .replace(regExForBold, '$1<b>$2</b>$3')
     .replace(
       regExForMention,
       `<a href="/profile/$1" style="color: #56a5ff; text-decoration: none;">@$1</a>$2`
