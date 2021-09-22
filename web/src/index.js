7f14214ae477d429800a0c6ca9d68ab4589186a7
KTachibanaM <whj19931115@gmail.com>
2021-09-22 08:40:22 -0700

    no need for frontend sentry

--- web/src/index.js
+++ web/src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import * as Sentry from "@sentry/react";
 import App from './App';
 import {unregister} from './registerServiceWorker';
-
-if (process.env.REACT_APP_SENTRY_DSN) {
-  console.log("Enable sentry")
-  Sentry.init({
-    dsn: process.env.REACT_APP_SENTRY_DSN
-  });
-} else {
-  console.log("Not enable sentry")
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
