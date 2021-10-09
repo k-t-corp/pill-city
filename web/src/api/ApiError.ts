5fdd8652883961172c6d085cc9af9030bc5ae9f1
KTachibanaM <whj19931115@gmail.com>
2021-10-09 08:30:54 -0700

    fix toast container css on mobile

--- web/src/api/ApiError.ts
+++ web/src/api/ApiError.ts
@@ -7,7 +7,7 @@ export default class ApiError extends Error {
     if (response.data && response.data.message) {
       this.message = `${response.data.message} (${response.status})`
     } else if (response.data) {
-      this.message = `${response.data} (${response.status})`
+      this.message = `${JSON.stringify(response.data)} (${response.status})`
     } else {
       this.message = `${response.statusText} (${response.status})`
     }
