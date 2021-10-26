1881bef983797fe99e1499fb794976cb48863c70
KTachibanaM <whj19931115@gmail.com>
2021-10-26 01:09:07 -0700

    small change

--- web/src/pages/SignUp/SignUp.js
+++ web/src/pages/SignUp/SignUp.js
@@ -141,7 +141,7 @@ export default class SignUp extends Component {
                     <Input
                       fluid
                       name='invitationCode'
-                      placeholder='Invitation code'
+                      placeholder='* Invitation code'
                       errorLabel={errorLabel}
                     />
                 }
