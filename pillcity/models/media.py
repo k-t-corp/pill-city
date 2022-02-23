ded387b25146c0305d9f77a1546c6fc846b2f234
KTachibanaM <whj19931115@gmail.com>
2022-02-23 00:59:55 -0800

    model change and backfill script for media ownership

--- pillcity/models/media.py
+++ pillcity/models/media.py
@@ -1,7 +1,14 @@
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, LazyReferenceField, IntField, DO_NOTHING
 from .created_at_mixin import CreatedAtMixin
 
 
 class Media(Document, CreatedAtMixin):
     # this is the object name (obviously..)
     id = StringField(primary_key=True)
+    # todo: change to required
+    # DO_NOTHING instead of NULLIFY here because of circular ref to User model
+    #   We should instead manually NULLIFY.
+    # Missing type because don't want circular ref
+    owner = LazyReferenceField('User', required=False, default=None, reverse_delete_rule=DO_NOTHING)
+    # todo: change to required
+    refs = IntField(required=False, default=-1)
--- pillcity/models/user.py
+++ pillcity/models/user.py
@@ -8,7 +8,7 @@ class User(Document, CreatedAtMixin):
     user_id = StringField(required=True, unique=True)
     password = StringField(required=True)
     followings = ListField(LazyReferenceField('User', reverse_delete_rule=PULL), default=[])  # type: List[User]
-    avatar = LazyReferenceField(Media, reverse_delete_rule=NULLIFY)
+    avatar = LazyReferenceField(Media, reverse_delete_rule=NULLIFY)  # type: Media
     profile_pic = StringField(required=False, default="pill1.png")
     display_name = StringField(required=False)
     email = EmailField(required=False)
