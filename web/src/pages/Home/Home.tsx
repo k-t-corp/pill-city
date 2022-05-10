c898e42bacf5c0e6c11958234e81694c79f4621a
KTachibanaM <whj19931115@gmail.com>
2022-05-10 00:56:02 -0700

    refactor home page

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -38,64 +38,63 @@ const Home = () => {
     }
   })
 
-  let homePostElement = () => {
-    if (loading || meLoading) {
-      return (<div className="home-status">Loading...</div>)
-    } else if (posts.length === 0) {
-      return (<div className="home-status">No posts here</div>)
-    } else {
-      let postElements = []
-      for (let i = 0; i < posts.length; i++) {
-        const post = posts[i]
-        postElements.push(
-          <div
-            // need to use post ID instead of index as key
-            // otherwise comments and reactions will be shifted after a new post is prepended
-            key={post.id}
-            ref={i === Math.floor(posts.length * InfiniteScrollFactor) - 1 ? observe : null}
-          >
-            <Post
-              data={post}
-              me={me as User}
-              detail={false}
-              hasNewPostModal={isTabletOrMobile}
-              updateResharePostData={updateResharePostData}
-              updateNewPostOpened={updateMobileNewPostOpened}
-            />
-          </div>
-        )
-      }
-      let endElem;
-      if (noMore) {
-        endElem = (
-          <div
-            key={posts.length}
-            className='home-load-more home-load-more-disabled'
-          >No more new posts</div>
-        )
-      }
-      else if (loadingMore) {
-        endElem = (
-          <div
-            key={posts.length}
-            className='home-load-more home-load-more-disabled'
-          >Loading...</div>
-        )
-      } else {
-        endElem = (
-          <div
-            key={posts.length}
-            className='home-load-more'
-            onClick={async () => {
-              await dispatch(loadMorePosts())
-            }}
-          >Load more</div>
-        )
-      }
-      postElements.push(endElem)
-      return postElements
-    }
+  if (loading || meLoading) {
+    return (<div className="home-status">Loading...</div>)
   }
+
+  if (posts.length === 0) {
+    return (<div className="home-status">No posts here</div>)
+  }
+
+  let postElements = []
+  for (let i = 0; i < posts.length; i++) {
+    const post = posts[i]
+    postElements.push(
+      <div
+        // need to use post ID instead of index as key
+        // otherwise comments and reactions will be shifted after a new post is prepended
+        key={post.id}
+        ref={i === Math.floor(posts.length * InfiniteScrollFactor) - 1 ? observe : null}
+      >
+        <Post
+          data={post}
+          me={me as User}
+          detail={false}
+          hasNewPostModal={isTabletOrMobile}
+          updateResharePostData={updateResharePostData}
+          updateNewPostOpened={updateMobileNewPostOpened}
+        />
+      </div>
+    )
+  }
+  let endElem;
+  if (noMore) {
+    endElem = (
+      <div
+        key={posts.length}
+        className='home-load-more home-load-more-disabled'
+      >No more new posts</div>
+    )
+  }
+  else if (loadingMore) {
+    endElem = (
+      <div
+        key={posts.length}
+        className='home-load-more home-load-more-disabled'
+      >Loading...</div>
+    )
+  } else {
+    endElem = (
+      <div
+        key={posts.length}
+        className='home-load-more'
+        onClick={async () => {
+          await dispatch(loadMorePosts())
+        }}
+      >Load more</div>
+    )
+  }
+  postElements.push(endElem)
 
   const newPostElem = (
     <NewPost
@@ -113,7 +112,7 @@ const Home = () => {
   return (
     <div className="home-wrapper">
       <div className="home-posts-wrapper">
-        {homePostElement()}
+        {postElements}
       </div>
       {isTabletOrMobile ?
         <>
