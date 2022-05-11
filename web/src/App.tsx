f00e320c279fb49d4b88081e812d62b6bb697d31
KTachibanaM <whj19931115@gmail.com>
2022-05-10 17:05:40 -0700

    remove web api doc

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -29,7 +29,6 @@ import Modal from "react-modal";
 import NavBar from "./components/NavBar/NavBar";
 import {accessTokenExists} from "./api/AuthStorage";
 import {getUseMultiColumn} from "./utils/SettingsStorage";
-import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 
 Modal.setAppElement('#root');
@@ -147,9 +146,6 @@ export default () => {
           </Route>
           <Route path="/reset">
             <ResetPassword />
-          </Route>
-          <Route path='/api'>
-            <SwaggerUI url="https://raw.githubusercontent.com/pill-city/spec/master/swagger.yaml"/>
           </Route>
           <Redirect to='/'/>
         </Switch>
