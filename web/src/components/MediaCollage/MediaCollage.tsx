736b64547f2bc04f63e5eb916d0e8e69ec5020a9
KTachibanaM <whj19931115@gmail.com>
2022-05-23 15:55:39 -0700

    use media collage for post media

---
+++ web/src/components/MediaCollage/MediaCollage.tsx
@@ -0,0 +1,129 @@
+import React, {useState} from "react";
+import './MediaCollage.css'
+import Lightbox from "react-image-lightbox";
+
+interface Props {
+  mediaUrls: string[]
+}
+
+export default (props: Props) => {
+  const {mediaUrls} = props
+
+  if (mediaUrls.length === 0) {
+    return null
+  }
+
+  const [showingMediaIndex, updateShowingMediaIndex] = useState(-1)
+  let elements: JSX.Element[] = []
+
+  const onClick = (i: number) => {
+    updateShowingMediaIndex(i)
+  }
+
+  if (mediaUrls.length === 1) {
+    elements = [
+      <div className='media-collage-img-container'>
+        <img className='media-collage-img' src={mediaUrls[0]} alt="" onClick={e => {
+          e.preventDefault()
+          onClick(0)
+        }}/>
+      </div>
+    ]
+  }
+
+  if (mediaUrls.length === 2) {
+    elements = [
+      <div className='media-collage-img-container'>
+        <img className='media-collage-img' src={mediaUrls[0]} alt="" onClick={e => {
+          e.preventDefault()
+          onClick(0)
+        }}/>
+      </div>,
+      <div className='media-collage-img-container'>
+        <img className='media-collage-img' src={mediaUrls[1]} alt="" onClick={e => {
+          e.preventDefault()
+          onClick(1)
+        }}/>
+      </div>
+    ]
+  }
+
+  if (mediaUrls.length === 3) {
+    elements = [
+      <div className='media-collage-img-container'>
+        <img className='media-collage-img' src={mediaUrls[0]} alt="" onClick={e => {
+          e.preventDefault()
+          onClick(0)
+        }}/>
+      </div>,
+      <div className='media-collage-img-col-container'>
+        <div className='media-collage-img-container media-collage-img-container-half'>
+          <img className='media-collage-img media-collage-img-half' src={mediaUrls[1]} alt="" onClick={e => {
+            e.preventDefault()
+            onClick(1)
+          }}/>
+        </div>
+        <div className='media-collage-img-container media-collage-img-container-half'>
+          <img className='media-collage-img media-collage-img-half' src={mediaUrls[2]} alt="" onClick={e => {
+            e.preventDefault()
+            onClick(2)
+          }}/>
+        </div>
+      </div>
+    ]
+  }
+
+  if (mediaUrls.length === 4) {
+    elements = [
+      <div className='media-collage-img-col-container'>
+        <div className='media-collage-img-container media-collage-img-container-half'>
+          <img className='media-collage-img media-collage-img-half' src={mediaUrls[0]} alt="" onClick={e => {
+            e.preventDefault()
+            onClick(0)
+          }}/>
+        </div>
+        <div className='media-collage-img-container media-collage-img-container-half'>
+          <img className='media-collage-img media-collage-img-half' src={mediaUrls[1]} alt="" onClick={e => {
+            e.preventDefault()
+            onClick(1)
+          }}/>
+        </div>
+      </div>,
+      <div className='media-collage-img-col-container'>
+        <div className='media-collage-img-container media-collage-img-container-half'>
+          <img className='media-collage-img media-collage-img-half' src={mediaUrls[2]} alt="" onClick={e => {
+            e.preventDefault()
+            onClick(2)
+          }}/>
+        </div>
+        <div className='media-collage-img-container media-collage-img-container-half'>
+          <img className='media-collage-img media-collage-img-half' src={mediaUrls[3]} alt="" onClick={e => {
+            e.preventDefault()
+            onClick(3)
+          }}/>
+        </div>
+      </div>
+    ]
+  }
+
+  return (
+    <div className='media-collage'>
+      {elements}
+      {showingMediaIndex !== -1 &&
+        <Lightbox
+          mainSrc={mediaUrls[showingMediaIndex]}
+          prevSrc={showingMediaIndex !== 0 ? mediaUrls[showingMediaIndex - 1] : undefined}
+          onMovePrevRequest={() =>
+            updateShowingMediaIndex(showingMediaIndex - 1)
+          }
+          nextSrc={showingMediaIndex !== mediaUrls.length - 1 ? mediaUrls[showingMediaIndex + 1] : undefined}
+          onMoveNextRequest={() =>
+            updateShowingMediaIndex(showingMediaIndex + 1)
+          }
+          onCloseRequest={() => {updateShowingMediaIndex(-1)}}
+          animationDuration={0}
+        />
+      }
+    </div>
+  )
+}
