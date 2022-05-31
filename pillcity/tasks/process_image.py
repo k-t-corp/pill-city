af9b92e15ec5f6610b0554192f9c3ea9693892c4
KTachibanaM <whj19931115@gmail.com>
2022-05-31 00:44:11 -0700

    abs out get s3 client

--- pillcity/tasks/process_image.py
+++ pillcity/tasks/process_image.py
@@ -10,6 +10,11 @@ def process_image(_id: str):
     logger.info(f"Processing image {_id}")
     media = Media.objects.get(id=_id)  # type: Media
     try:
+        # download the image from s3
+        # compute metadata
+        # convert to resized webp
+        # upload resized webp to s3
+        # set media as processed
         pass
     except Exception as e:
         logger.warn(str(e))
