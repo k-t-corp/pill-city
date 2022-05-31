d95d813f44c8f7141b05cb0f17eff9bd968090d5
KTachibanaM <whj19931115@gmail.com>
2022-05-31 00:55:32 -0700

    trigger the async job and download the image in async job

--- pillcity/tasks/celery.py
+++ pillcity/tasks/celery.py
@@ -7,7 +7,8 @@ app = celery.Celery(
     'tasks',
     broker=os.environ['REDIS_URL'],
     include=[
-        'pillcity.tasks.generate_link_preview'
+        'pillcity.tasks.generate_link_preview',
+        'pillcity.tasks.process_image'
     ]
 )
 logger = get_task_logger(__name__)
--- pillcity/tasks/process_image.py
+++ pillcity/tasks/process_image.py
@@ -1,6 +1,9 @@
 import os
+import uuid
+import tempfile
 from mongoengine import connect, disconnect
 from pillcity.models import Media
+from pillcity.utils.s3 import get_s3_client
 from .celery import app, logger
 
 
@@ -11,6 +14,11 @@ def process_image(_id: str):
     media = Media.objects.get(id=_id)  # type: Media
     try:
         # download the image from s3
+        temp_fp = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+        s3_client, s3_bucket_name = get_s3_client()
+        with open(temp_fp, 'wb') as f:
+            s3_client.download_fileobj(s3_bucket_name, media.id, f)
+
         # compute metadata
         # convert to resized webp
         # upload resized webp to s3
