9ab0142d5b5d96beb1d398db575a2e8a0331dbe6
KTachibanaM <whj19931115@gmail.com>
2022-06-03 01:59:09 -0700

    view original button

--- web/src/components/MediaCollage/MediaCollage.tsx
+++ web/src/components/MediaCollage/MediaCollage.tsx
@@ -125,6 +125,9 @@ export default (props: Props) => {
           }
           onCloseRequest={() => {updateShowingMediaIndex(-1)}}
           animationDuration={0}
+          toolbarButtons={[
+            <a href={mediaUrls[showingMediaIndex].original_url}>Original</a>
+          ]}
         />
       }
     </div>
