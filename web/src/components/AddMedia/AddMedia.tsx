3a3c84895a8492025e6f376889ca20f349679190
KTachibanaM <whj19931115@gmail.com>
2022-12-31 02:47:51 -0800

    temporarily remove use ownded media while uploading

--- web/src/components/AddMedia/AddMedia.tsx
+++ web/src/components/AddMedia/AddMedia.tsx
@@ -1,5 +1,4 @@
-import React, {useState} from "react";
-import OwnedMedia from "./OwnedMedia";
+import React from "react";
 import Media from "../../models/Media"
 import UploadMedia from "./UploadMedia";
 import './AddMedia.css'
@@ -11,37 +10,10 @@ interface Props {
 }
 
 export default (props: Props) => {
-  const [showingTab, updateShowingTab] = useState(0)
-
   return (
-    <>
-      <div className='add-media-tabs'>
-        <div
-          className={'add-media-tab' + (showingTab === 0 ? ' add-media-tab-selected' : '')}
-          onClick={() => {updateShowingTab(0)}}
-        >Upload new media</div>
-        <div
-          className={'add-media-tab' + (showingTab === 1 ? ' add-media-tab-selected' : '')}
-          onClick={() => {updateShowingTab(1)}}
-        >Use uploaded media</div>
-      </div>
-      <div>
-        {showingTab === 0 &&
-          <UploadMedia
-            onChangeMedias={props.onChangeMedias}
-            onClose={props.onClose}
-          />
-        }
-        {showingTab === 1 &&
-          <OwnedMedia
-            selectMediaOp={'Use'}
-            onSelectOwnedMedia={m => {
-              props.onSelectOwnedMedia(m)
-              props.onClose()
-            }}
-          />
-        }
-      </div>
-    </>
+    <UploadMedia
+      onChangeMedias={props.onChangeMedias}
+      onClose={props.onClose}
+    />
   )
 }
